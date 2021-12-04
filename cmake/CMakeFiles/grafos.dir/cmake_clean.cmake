492bf5c0788c28249f9e5121141d3f5f55ff857f
camistorni <cstorni@fi.uba.ar>
2021-12-04 12:55:20 -0300

    hola

---
+++ cmake/CMakeFiles/grafos.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grafos.dir/CaminoMinimo.cpp.o"
+  "CMakeFiles/grafos.dir/Dijkstra.cpp.o"
+  "CMakeFiles/grafos.dir/Floyd.cpp.o"
+  "CMakeFiles/grafos.dir/Grafo.cpp.o"
+  "CMakeFiles/grafos.dir/Vertice.cpp.o"
+  "CMakeFiles/grafos.dir/main.cpp.o"
+  "grafos"
+  "grafos.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grafos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
